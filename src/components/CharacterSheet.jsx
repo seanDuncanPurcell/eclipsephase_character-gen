4b3016f672be7b74118c8229531b6ef7fe4102cb
Sean D Purcell <sean.d.purcell@gmail.com>
2021-03-01 20:53:17 -0800

    solved jsx input bug

--- src/components/CharacterSheet.jsx
+++ src/components/CharacterSheet.jsx
@@ -1,58 +1,128 @@
 import React,{Component} from 'react';
-
-const rpdata = {
+//this data block will be dirved from the rule book and can not be altered by players
+const staticData = {
   playerName: {        
-    lable: 'Player Name',
+    label: 'Player Name',
     id: 'plynam01',
     type: 'text',
     value: ''
   },
   characterName: {        
-    lable: 'Character Name',
+    label: 'Character Name',
     id: 'charnam02',
     type: 'text',
     value: ''
   },
   subjectiveAge: {        
-    lable: 'Subjective Age',
+    label: 'Subjective Age',
     id: 'fisihghari65644f34s6',
     type: 'text',
     value: ''
   },
-  objjectiveAge: {        
-    lable: 'Objective Age',
+  objectiveAge: {        
+    label: 'Objective Age',
     id: '45g45g3sadfg5f15s3',
     type: 'text',
     value: ''
   },
   sleeve: {        
-    lable: 'Sleeve',
+    label: 'Sleeve',
     type: 'dropDown',
-    list: [{name: 'Grade 1', id: 'sleeve001'}, {name: 'Grade 2', id: 'sleeve002'}, {name: 'Grade 3', id: 'sleeve004'}],
-    value: ''
+    options: {
+      default: {
+        label: 'Select One',
+        aptitudesMods: {
+          cog: -1,
+          coo: -1,
+          int: -1,
+          ref: -1,
+          sav: -1,
+          som: -1,
+          wil: -1,
+        },
+        cost: 0
+      }, 
+      normal: {
+        label: 'Normal',
+        aptitudesMods: {
+          cog: 0,
+          coo: 0,
+          int: 0,
+          ref: 0,
+          sav: 0,
+          som: 0,
+          wil: 0,
+        },
+        cost: 0
+      }, 
+      gradeThree: {
+        label: 'Grade 3',
+        aptitudesMods: {
+          cog: 3,
+          coo: 0,
+          int: 0,
+          ref: 0,
+          sav: 0,
+          som: 3,
+          wil: 0,
+        },
+        cost: 10
+      }, 
+      gradeFour: {
+        label: 'Grade 4',
+        aptitudesMods: {
+          cog: 5,
+          coo: 0,
+          int: 0,
+          ref: 0,
+          sav: 0,
+          som: 5,
+          wil: 0,
+        },
+        cost: 20
+      }, 
+    }
   },
   backGround: {        
-    lable: 'Background',
+    label: 'Background',
     type: 'dropDown',
-    list: [{name: 'Street Kid', id: 'bg001'}, {name: 'Corpo', id: 'bg002'}, {name: 'Nomad', id: 'bg003'}],
-    value: ''
+    options: {
+      default: {label: 'Select One'}, 
+      streetKid: {label: 'Street Kid'}, 
+      corpo: {label: 'Corpo'}, 
+      nomad: {label: 'Nomad'}
+    }
   },
   faction: {        
-    lable: 'Faction',
+    label: 'Faction',
     type: 'dropDown',
-    list: [{name: 'Red', id: 'faction001'}, {name: 'Blue', id: 'faction002'}, {name: 'Green', id: 'faction003'}],
-    value: ''
+    options: {
+      default: {label: 'Select One'}, 
+      red: {label: 'Red Team'}, 
+      blue: {label: 'Blue Team'}, 
+      green: {label: 'Green Team'}
+    }
   },
 }
 
-const characterAptitudes = {
-  cog: {name: 'Cognition', abbreviation: 'COG', value: 5, morphBonus: 0, total: 0},
-  coo: {name: 'Coordination', abbreviation: 'COO', value: 5, morphBonus: 0, total: 0},
-  int: {name: 'Intuition', abbreviation: 'INT', value: 5, morphBonus: 0, total: 0},
-  ref: {name: 'Reflexes', abbreviation: 'REF', value: 5, morphBonus: 0, total: 0},
-  sav: {name: 'Savvy', abbreviation: 'SAV', value: 5, morphBonus: 0, total: 0},
-  som: {name: 'Somatics', abbreviation: 'SOM', value: 5, morphBonus: 0, total: 0},
-  wil: {name: 'Willpower', abbreviation: 'WIL', value: 5, morphBonus: 0, total: 0}
+//this data black will be defined by the static date but the values will be set by user, saved to the DB, and then loard back.
+const dynamicData = {
+  playerName: '',
+  characterName: '',
+  subjectiveAge: '',
+  objectiveAge: '',
+  aptitudes: {
+    cog: {name: 'Cognition', abbreviation: 'COG', value: 5, morphBonus: 0, total: 5},
+    coo: {name: 'Coordination', abbreviation: 'COO', value: 5, morphBonus: 0, total: 5},
+    int: {name: 'Intuition', abbreviation: 'INT', value: 5, morphBonus: 0, total: 5},
+    ref: {name: 'Reflexes', abbreviation: 'REF', value: 5, morphBonus: 0, total: 5},
+    sav: {name: 'Savvy', abbreviation: 'SAV', value: 5, morphBonus: 0, total: 5},
+    som: {name: 'Somatics', abbreviation: 'SOM', value: 5, morphBonus: 0, total: 5},
+    wil: {name: 'Willpower', abbreviation: 'WIL', value: 5, morphBonus: 0, total: 5}    
+  },
+  sleeve: 'default',
+  backGround: 'default',
+  faction: 'default'
 }
 
 function keyGen() {
@@ -61,172 +131,135 @@ function keyGen() {
 }
 
 //RPData Sub-Components
-class DropDown extends Component{
-  constructor (props){
-    super(props)
-    this.state = {value: this.props.value}
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(event){
-    const newValue = event.target.value
-    this.setState({value: newValue})
-    this.props.liftState(newValue);
-  }
-  
-  render(){
-    return(
-      <label key={keyGen()} >
-        <p key={keyGen()} >{this.props.children}</p>
-        <select onChange={this.handleInputChange} key={keyGen()} value={this.state.value}>
-          {this.props.list.map(item => {
-            return <option value={item.name} key={keyGen()}>{item.name}</option>
-          })}
-        </select>
-      </label>
-    )
-  }
+function SimpleField (props) {
+  const autoFocus = (props.autoFocus) ? true : false;
+  const autoComplete = (props.autoComplete) ? 'on' : 'off';
+  return(
+    <label>
+      <p>{props.label}</p>
+      <input 
+        type={props.type}
+        value={props.value}
+        onChange={props.onChange}
+        autoComplete={autoComplete}
+        autoFocus={autoFocus}
+      ></input>
+    </label>
+  )
 }
 
-class SimpleField extends Component{
-  constructor (props){
-    super(props)
-    this.state = {value: this.props.value};
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(event){
-    const newValue = event.target.value;
-    this.setState({value: newValue});
-    this.props.liftState(newValue);
-  }
-  
-  render(){
-    return(
-      <label>
-        <p>{this.props.children}</p>
-        <input 
-          type={this.props.type}
-          autoComplete='off'
-          onChange={this.handleInputChange}   
-          value={this.state.value}
-        ></input>
-      </label>
-    )
-  }
-}
-
-class RPData extends Component{
-  constructor(props){
-    super(props);
-    this.handleLiftState = this.handleLiftState.bind(this);
-  }
-
-  handleLiftState(value, key){
-    const newData = this.props.data
-    newData[key].value = value;
-    this.props.liftState(newData);
-  }
-
-  render(){
-    const myData = this.props.data;
-    return(
-      <section className='RPData' key={keyGen()}>
-        {Object.keys(myData).map(key => {
-          if (myData[key].type === 'text')
-            return (
-              <SimpleField
-                type='text'
-                value={myData[key].value}
-                liftState={(value)=>this.handleLiftState(value, key)}
-                key={keyGen()}
-              >{myData[key].lable}</SimpleField>
-            )
-          else if (myData[key].type === 'dropDown')
-            return (
-              <DropDown 
-                list={myData[key].list}
-                liftState={(value)=>this.handleLiftState(value, key)}
-                key={keyGen()}
-                value={myData[key].value}
-              >{myData[key].lable}</DropDown>
-            )
-          else 
-            return <p>render fail</p>
+function SimpleDropDown (props){
+  return(
+    <label>
+      <p>{props.label}</p>
+      <select onChange={props.onChange} value={props.value}>
+        {Object.keys(props.options).map(key => {
+          const item = props.options[key];
+          return <option value={key} key={keyGen()}>{item.label}</option>
         })}
-      </section>
-    )
-  }
+      </select>
+    </label>
+  )
 }
 
-//Aptitudes
-class Aptitudes extends Component{
-  constructor(props){
-    super(props)
-    this.state = this.props.data;
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
+function Aptitudes (props) {
+  return(
+    <section className='character-aptitudes'>
+      {Object.keys(props.data).map((key) => {
 
-  handleInputChange(event, key){
-    const value = event.target.value;
-    const newData = this.state
-    newData[key].value = parseInt(value, 10);
-    this.setState(newData);
-    this.props.liftState(newData);
-  }
+        //pull respective data point for this element
+        const myData = props.data[key];
 
-  render(){
-    return(
-      <section className='character-aptitudes'>
-        {Object.keys(this.state).map((key) => {
-          return (
-            <label className='single-aptitude' key={key}>
-              <p className='tooltip'>
-                {this.state[key].abbreviation}
-                <span className='tooltiptext' key={keyGen()}>{this.state[key].name}</span>
-              </p>
-              <input
-                className='aptitudes-input'
-                type='number' 
-                min='5' max='25' 
-                value={this.state[key].value}
-                step='1'
-                onChange={ event => this.handleInputChange(event, key)}
-              />
-              <p>{this.state[key].morphBonus}</p>
-              <p>{this.state[key].total}</p>
-            </label>            
-          )
-        } ) }
-      </section>
-    )
-  }
+        //return sub-component
+        return (
+          <label 
+            className='single-aptitude' 
+            key={['aptitudes', key].join('_')} 
+          >
+            <p className='tooltip'>
+              {myData.abbreviation}
+              <span className='tooltiptext'>{myData.name}</span>
+            </p>
+            <input
+              className='aptitudes-input'
+              type='number' 
+              min='5' max='25' 
+              value={myData.value}
+              step='1'
+              onChange={event => props.handleChange(event, key, 'value')}
+            />
+            <p>{myData.morphBonus}</p>
+            <p>{myData.total}</p>
+          </label>            
+        )
+
+      } ) }
+    </section>
+  )
 }
 
-//Skills, Active
-
-//Skills, Knowledge
-
-//Morphs
-
-//Main Component
 class CharacterSheet extends Component {
   constructor(props){
-    super(props)
-    this.characterSheetData = {rpData: rpdata, charApts: characterAptitudes}
-    this.handleLiftData = this.handleLiftData.bind(this);
+    super(props);
+    this.state = dynamicData;
+    this.staticData = staticData;
+
+    this.handleInputChange = this.handleInputChange.bind(this);
   }
 
-  handleLiftData(data, key) {
-    this.characterSheetData[key] = data;
+  handleInputChange(event, ...keys){
+    const isNum = parseInt(event.target.value, 10)
+    const value = isNum ? isNum : event.target.value ;
+    let newState = this.state;
+
+    for(let i = 0, state = newState; i < keys.length; i++){
+      if(!keys[(i + 1)]) {
+        state[keys[i]] = value;
+      }else{
+        state = state[keys[i]];
+      }
+    }
+
+    this.setState(newState);
   }
 
   render(){
+    const formMap = this.staticData;
     return(
       <>
-        <RPData liftState={data=>this.handleLiftData(data, 'rpData')} data={this.characterSheetData.rpData}/>
-        <Aptitudes liftState={data=>this.handleLiftData(data, 'charApts')} data={this.characterSheetData.charApts}/>
-        <button onClick={()=>{console.log(this.characterSheetData)}}>Report</button>
+        {Object.keys(formMap).map( key => {
+          const item = formMap[key];
+          if (item.type === 'text') {
+            return (
+              <SimpleField
+                type={item.type}
+                label={item.label}
+                value={this.state[key]}
+                onChange={event => this.handleInputChange(event, key)}
+                key={[item.type, key].join('_')}
+              />
+            )
+          } else if (item.type === 'dropDown') {
+            return (
+              <SimpleDropDown
+                label={item.label}
+                options={item.options}
+                value={this.state[key]}
+                onChange={event => this.handleInputChange(event, key)}                
+                key={[item.type, key].join('_')}
+              />
+            )
+          } else {
+            return <p>Failed to render element</p>
+          }
+        })}
+        <Aptitudes 
+          data={this.state.aptitudes}
+          handleChange={(event, keyOne, keyTwo ) => this.handleInputChange(event, 'aptitudes', keyOne, keyTwo)}
+        />
+        <button onClick={()=>{
+          console.log(this.state);
+        }}>Report</button>
       </>
     )
   }
