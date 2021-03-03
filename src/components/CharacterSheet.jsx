65e3f7162a73042dc5c646afc9dd7a769bf78f5a
Duncan <sean.d.purcell@gmail.com>
2021-03-03 13:20:56 -0800

    template skills added

--- src/components/CharacterSheet.jsx
+++ src/components/CharacterSheet.jsx
@@ -32,14 +32,15 @@ const staticData = {
       default: {
         label: 'Select One',
         aptitudesMods: {
-          cog: -1,
-          coo: -1,
-          int: -1,
-          ref: -1,
-          sav: -1,
-          som: -1,
-          wil: -1,
+          cog: 0,
+          coo: 0,
+          int: 0,
+          ref: 0,
+          sav: 0,
+          som: 0,
+          wil: 0,
         },
+        aptitudesMax: 0,
         skillMods: {
           animalHandling: 0,
         },
@@ -56,6 +57,7 @@ const staticData = {
           som: 0,
           wil: 0,
         },
+        aptitudesMax: 25,
         skillMods: {
           climbing: +10
         },
@@ -72,6 +74,7 @@ const staticData = {
           som: 3,
           wil: 0,
         },
+        aptitudesMax: 30,
         skillMods: {
           animalHandling: +10,
           disguise: +5
@@ -89,6 +92,7 @@ const staticData = {
           som: 5,
           wil: 0,
         },
+        aptitudesMax: 40,
         skillMods: {
           blades: +10,
           climbing: +5,
@@ -102,10 +106,49 @@ const staticData = {
     label: 'Background',
     type: 'dropDown',
     options: {
-      default: {label: 'Select One'}, 
-      streetKid: {label: 'Street Kid'}, 
-      corpo: {label: 'Corpo'}, 
-      nomad: {label: 'Nomad'}
+      default: {
+        label: 'Select One'
+      },
+      drifter: {
+        label: 'Drifter',
+        description: `You were raised with a social grouping that remained
+        on the move throughout the Sol system. This could
+        have been free traders, pirates, asteroid farmers,
+        scavengers, or just migrant workers. You are used to
+        roaming space travel between habitats and stations.`,
+        skillMod: {
+          navigation: {mod: 10},
+          pilot: {mod: 10, subSkills: ['Spacecraft']},
+          netorking: {
+            mod: 10, 
+            subSkills: [
+              'Autonomists', 'Criminals', 'Ecologists', 'Firewall', 
+              'Hypercorps', 'Media', 'Scientists'
+            ]
+          }          
+        }
+      },
+      fallEvacuee: {
+        label: 'Fall Evacuee',
+        description: `You were born and raised on Earth and evacuated
+        during the horrors of the Fall, leaving your old life
+        (and possibly your friends, family, and loved ones)
+        behind you. You were lucky enough to survive with
+        your body intact and continue to make a life for yourself
+        out in the system.`,
+        skillMod: {
+          pilot: {mod: 10, subSkills: ['Groundcraft']},
+          netorking: {
+            mod: 10, 
+            subSkills: [
+              'Autonomists', 'Criminals', 'Ecologists', 'Firewall', 
+              'Hypercorps', 'Media', 'Scientists'
+            ]
+          }
+        },
+        moxieMod: 1,
+        creditMod: 2500
+      },
     }
   },
   faction: {        
@@ -146,7 +189,174 @@ const staticData = {
     deception: {
       lable: 'Deception',
       aptitudeKey: 'sav'
-    }
+    },
+    demolitions: {
+      lable: 'Demolitions',
+      aptitudeKey: 'cog'
+    },
+    disguise: {
+      lable: 'Disguise',
+      aptitudeKey: 'int'
+    },
+    exoticMelee: {
+      lable: 'Exotic Melee',
+      aptitudeKey: 'som',
+      options: ['Morning-Star', 'Spear', 'Whip']
+    },
+    exoticRanged: {
+      lable: 'Exotic Ranged',
+      aptitudeKey: 'coo',
+      options: ['Blowgun', 'Crossbow', 'Slingshot']
+    },
+    flight: {
+      lable: 'Flight',
+      aptitudeKey: 'som'
+    },
+    fray: {
+      lable: 'Fray',
+      aptitudeKey: 'ref'
+    },
+    freeFall: {
+      lable: 'Free Fall',
+      aptitudeKey: 'ref'
+    },
+    freeRunning: {
+      lable: 'Freerunning',
+      aptitudeKey: 'som'
+    },
+    gunnery: {
+      lable: 'Gunnery',
+      aptitudeKey: 'int'
+    },
+    hardware: {
+      lable: 'Hardware',
+      aptitudeKey: 'cog',
+      options: [
+        'Aerospace', 'Armorer', 'Electronics', 'Groundcraft', 
+        'Implants', 'Industrial', 'Nautical', 'Robotics'
+      ]
+    },
+    impersonatoin: {
+      lable: 'Impersonation',
+      aptitudeKey: 'sav'
+    },
+    infiltration: {
+      lable: 'Infiltration',
+      aptitudeKey: 'coo'
+    },
+    infoSec: {
+      lable: 'Infosec',
+      aptitudeKey: 'cog'
+    },
+    interface: {
+      lable: 'Interfacing',
+      aptitudeKey: 'cog'
+    },
+    intimidation: {
+      lable: 'Intimidation',
+      aptitudeKey: 'sav'
+    },
+    investigation: {
+      lable: 'Investigation',
+      aptitudeKey: 'int'
+    },
+    kinesice: {
+      lable: 'Kinesics',
+      aptitudeKey: 'sav'
+    },
+    kineticWeapons: {
+      lable: 'Kinetic Weapons',
+      aptitudeKey: 'coo'
+    },
+    medicine: {
+      lable: 'Medicine',
+      aptitudeKey: 'cog',
+      options: [
+        'Biosculpting', 'Exotic-Biomorphs', 'Gene-Therapy,', 'General-Practice', 
+        'Implant-Surgery', 'Nanomedicine', 'Paramedic', 'Pods', 'Psychiatry', 'Remote-Surgery', 
+        'Trauma-Surgery', 'Uplifts', 'Veterinary'
+      ]
+    },
+    navigation: {
+      lable: 'Navigation',
+      aptitudeKey: 'int'
+    },
+    networking: {
+      lable: 'Networking',
+      aptitudeKey: 'sav',
+      options: [
+        'Autonomists', 'Criminals', 'Ecologists', 'Firewall', 
+        'Hypercorps', 'Media', 'Scientists'
+      ]
+    },
+    palming: {
+      lable: 'Palming',
+      aptitudeKey: 'coo'
+    },
+    perception: {
+      lable: 'Perception',
+      aptitudeKey: 'int'
+    },
+    persuasion: {
+      lable: 'Persuasion',
+      aptitudeKey: 'sav'
+    },
+    pilot: {
+      lable: 'Pilot',
+      aptitudeKey: 'ref',
+      options: [
+        'Aircraft', 'Anthroform', 'Exotic-Vehicle', 'Groundcraft-tracked',
+        'Groundcraft-wheeled', 'Spacecraft', 'Watercraft'
+      ]
+    },
+    programming: {
+      lable: 'programming',
+      aptitudeKey: 'cog'
+    },
+    protocol: {
+      lable: 'protocol',
+      aptitudeKey: 'sav'
+    },
+    psiAssault: {
+      lable: 'Psi Assault',
+      aptitudeKey: 'wil'
+    },
+    psychosurgery: {
+      lable: 'Psychosurgery',
+      aptitudeKey: 'int'
+    },
+    research: {
+      lable: 'Research',
+      aptitudeKey: 'cog'
+    },
+    scrounging: {
+      lable: 'Scrounging',
+      aptitudeKey: 'int'
+    },
+    seekerWeapons: {
+      lable: 'Seeker Weapons',
+      aptitudeKey: 'coo'
+    },
+    sence: {
+      lable: 'Sence',
+      aptitudeKey: 'int'
+    },
+    sprayWeapons: {
+      lable: 'Spray Weapons',
+      aptitudeKey: 'coo'
+    },
+    swimming: {
+      lable: 'Swimming',
+      aptitudeKey: 'som'
+    },
+    thrownWeapons: {
+      lable: 'Thrown Weapons',
+      aptitudeKey: 'coo'
+    },
+    unarmedConbat: {
+      lable: 'Unarmed Combat',
+      aptitudeKey: 'som'
+    },
   }
 }
 
@@ -165,17 +375,12 @@ const dynamicData = {
     som: {name: 'Somatics', abbreviation: 'SOM', value: 5, morphBonus: 0, total: 5},
     wil: {name: 'Willpower', abbreviation: 'WIL', value: 5, morphBonus: 0, total: 5}    
   },
+  moxie: 0,
   sleeve: 'default',
   backGround: 'default',
   faction: 'default',
   skills: {
-    animalHandling: 0,
-    beamWeapons: 0,
-    blades:  0,
-    climbing: 0,
-    clubs: 0,
-    constrol: 0,
-    deception: 0
+    exoticMelee: { Spear: 4 }
   }
 }
 
@@ -217,14 +422,18 @@ function SimpleDropDown (props){
 }
 
 function Aptitudes (props) {
+  const fixed = props.dataStatic;
+  const dynamic = props.dataDynmic;
   return(
     <section className='character-aptitudes'>
-      {Object.keys(props.data).map((key) => {
-
-        //pull respective data point for this element
-        const myData = props.data[key];
-
-        //return sub-component
+      {Object.keys(dynamic.aptitudes).map((key) => {
+        const maxApt = fixed.sleeve.options[dynamic.sleeve].aptitudesMax
+        const myData = dynamic.aptitudes[key];
+        let alertStyle = {fontWeight: 'normal', color: 'black'};
+        if(maxApt < myData.total){
+          alertStyle = {fontWeight: 'bold', color: 'red'};
+        }
+          
         return (
           <label 
             className='single-aptitude' 
@@ -243,6 +452,7 @@ function Aptitudes (props) {
               onChange={event => props.handleChange(event, key, 'value')}
             />
             <p>{myData.morphBonus}</p>
+            <p style={alertStyle}>{maxApt}</p>
             <p>{myData.total}</p>
           </label>            
         )
@@ -254,56 +464,171 @@ function Aptitudes (props) {
         <p>Aptitudes</p>
         <p>Base Score</p>
         <p>Sleeve Mod</p>
+        <p>Sleeve Max</p>
         <p>Total</p>
       </label>   
     </section>
   )
 }
 
-function Skill (props){
-  const fixed = props.dataStatic;
-  const dynamic = props.dataDynmic;
-  return (
-    <section className='skill_block'>
-      <label className='skill_row skill_header'>
-        <p className='skill_text' >Skill Name</p>
-        <p className='skill_text' >Aptitude</p>
-        <p className='skill_num' >Total</p>
-        <p className='skill_num' >Ranks</p>
-        <p className='skill_num' >Apt. Mod</p>
-        <p className='skill_num' >Sleeve Mod</p>
+class Skills extends Component{
+  constructor(props){
+    super(props)
+    this.SimpleSkill = this.SimpleSkill.bind(this)
+    this.TemplateSkill = this.TemplateSkill.bind(this)
+  }
+  SimpleSkill(props){
+    const skillKey = props.skillKey
+    const fixedSkill = props.fixedSkill
+    const dataDynmic = props.dataDynmic
+    const dataStatic = props.dataStatic
+    const rank = props.rank
+
+    const aptMod = (()=>{
+      const aptitude = dataDynmic.aptitudes[fixedSkill.aptitudeKey].total
+      const morphMax = dataStatic.sleeve.options[dataDynmic.sleeve].aptitudesMax
+      return (aptitude > morphMax) ? morphMax : aptitude
+    })()
+    const specialMod = 0 //this is a place holder for faction/background bonuses TBD
+    const total = rank + aptMod + specialMod
+  
+    return (
+      <label 
+        className='skill_row'
+        key={['skill', skillKey].join('_')}
+      >
+        <p className='skill_text' >
+          {fixedSkill.lable}
+        </p>
+        <p className='skill_cell' >{fixedSkill.aptitudeKey.toUpperCase()}</p>
+        <p className='skill_cell' >{total}</p>
+        <input
+          className='skill_input'
+          type='number'
+          min='0' max='60'
+          value={rank}
+          step='5'
+          onChange={event => props.handleChange(event, skillKey)}
+          key={['skill', skillKey, 'input'].join('_')}
+        />
+        <p className='skill_cell' >{aptMod}</p>
+        <p className='skill_cell' >{specialMod}</p>
       </label>
-      {Object.keys(fixed.skills).map( skill =>{
-        const item = fixed.skills[skill];
-        const itemRank = dynamic.skills[skill];
-        const aptitudeBonus = dynamic.aptitudes[item.aptitudeKey].total;
-        const morphMod = fixed.sleeve.options[dynamic.sleeve].skillMods[skill];
-        const morphBonus =  morphMod ? morphMod : 0 ;
-        const itemTotal = itemRank + aptitudeBonus + morphBonus;
-        return(
-          <label 
-            className='skill_row'
-            key={['skill', skill].join('_')}
-          >
-            <p className='skill_text' >{item.lable}</p>
-            <p className='skill_text' >{item.aptitudeKey.toUpperCase()}</p>
-            <p className='skill_num' >{itemTotal}</p>
-            <input
-              className='skill_input'
-              type='number' 
-              min='0' max='60' 
-              value={itemRank}
-              step='5'
-              onChange={event => props.handleChange(event, skill)}
-              key={['skill', skill, 'input'].join('_')}
+    )      
+  }
+  TemplateSkill(props){
+    const skillKey = props.skillKey
+    const fixedSkill = props.fixedSkill
+    const dataDynmic = props.dataDynmic
+    const dataStatic = props.dataStatic
+    const subSkills = fixedSkill.options.filter( _key => {
+      let charSubSkills = dataDynmic.skills[skillKey]
+      if(charSubSkills){
+        charSubSkills = charSubSkills[_key]
+        if(charSubSkills >= 0){
+          return true
+        }
+      } else {
+        return false
+      }
+    })
+
+    return (
+      <>
+        <label
+          className='skill_row'
+          key={['skill', skillKey].join('_')}
+        >
+          <div className='skill_template-lable' >
+            <p className='skill_text' >
+              {fixedSkill.lable}: 
+            </p>
+            <select value='default' onChange={(event) => props.handleNewSubSkill( event, skillKey )}>
+                <option value='default'>Select One</option>
+              {fixedSkill.options.map( option => {
+                return( 
+                  <option value={option} key={['option', skillKey, option ].join('_')}>
+                    {option}
+                  </option>
+                )
+              })}
+            </select>
+          </div>
+          <p className='skill_cell' >{fixedSkill.aptitudeKey.toUpperCase()}</p>
+          <p className='skill_cell' />
+          <p className='skill_cell' />
+          <p className='skill_cell' />
+          <p className='skill_cell' />
+        </label>
+        {subSkills.map( subSkillKey => {
+          const thisSkill = {lable: `${fixedSkill.lable}: ${subSkillKey}`, aptitudeKey:fixedSkill.aptitudeKey}
+          const rank = dataDynmic.skills[skillKey][subSkillKey]
+          return(
+            <this.SimpleSkill
+              fixedSkill={thisSkill}
+              rank={rank}
+              skillKey={subSkillKey}
+              dataStatic={dataStatic}
+              dataDynmic={dataDynmic}
+              handleChange={(event, subKey) => props.handleChange(event, skillKey, subKey)}
+              key={['over-skill', skillKey, subSkillKey].join('_')}
             />
-            <p className='skill_num' >{aptitudeBonus}</p>
-            <p className='skill_num' >{morphBonus}</p>
-          </label>
-        )
-      })}      
-    </section>
-  )
+          )          
+        })}
+      </>
+    )
+  }
+  render(){
+    const skillList = this.props.dataStatic.skills
+    const dataDynmic = this.props.dataDynmic
+    return(
+      <section className='skill_block' >
+        <label 
+          className='skill_row skill_header'          
+          key={['over-skill', 'header'].join('_')}
+        >
+          <p className='skill_text' >Skill Name</p>
+          <p className='skill_cell' >Aptitude</p>
+          <p className='skill_cell' >Total</p>
+          <p className='skill_cell' >Ranks</p>
+          <p className='skill_cell' >Apt. Mod</p>
+          <p className='skill_cell' >Special Bonuses</p>
+        </label>
+        {Object.keys(skillList).map( skillKey => {
+          const fixedSkill = skillList[skillKey];
+
+          //If Normal Skill then render a SimpleSkill
+          if(!fixedSkill.options){
+            const rank = (dataDynmic.skills[skillKey]) ? dataDynmic.skills[skillKey] : 0
+            return(
+              <this.SimpleSkill
+                fixedSkill={fixedSkill}
+                rank={rank}
+                skillKey={skillKey}
+                dataStatic={this.props.dataStatic}
+                dataDynmic={dataDynmic}
+                handleChange={this.props.handleChange}
+                key={['over-skill', skillKey].join('_')}
+              />
+            )
+          }//Otherwise its a TemplateSkill 
+          else{
+            return(
+              <this.TemplateSkill 
+                fixedSkill={fixedSkill}
+                skillKey={skillKey}
+                dataStatic={this.props.dataStatic}
+                dataDynmic={dataDynmic}
+                handleChange={this.props.handleChange}
+                handleNewSubSkill={this.props.handleNewSubSkill}
+                key={['over-skill', skillKey].join('_')}
+              />
+            )
+          }
+        })}
+      </section>
+    )
+  }
 }
 
 class CharacterSheet extends Component {
@@ -313,11 +638,31 @@ class CharacterSheet extends Component {
     this.staticData = staticData;
 
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleNewSubSkill = this.handleNewSubSkill.bind(this);
+  }
+
+  handleNewSubSkill(event, skill){
+    const subSkill = event.target.value
+    const data = this.state
+    if(data.skills[skill]){
+      if(data.skills[skill][subSkill] >= 0){
+        return
+      }else{
+        data.skills[skill][subSkill] = 0
+      }
+    }else{
+      data.skills[skill] = {}
+      data.skills[skill][subSkill] = 0
+    }
+    this.setState(data)
   }
 
   handleInputChange(event, ...keys){
-    const isNum = parseInt(event.target.value, 10)
-    const value = isNum ? isNum : event.target.value ;
+    const isNum = parseInt(event.target.value, 10);
+    // const isObj = JSON.parse(event.target.value);
+    let value = event.target.value;
+    if (isNum || isNum === 0) value = isNum;
+    // else if (isObj) value = isObj;
     let newState = this.state;
 
     for(let i = 0, state = newState; i < keys.length; i++){
@@ -398,14 +743,18 @@ class CharacterSheet extends Component {
         })}
         <br/>
         <Aptitudes 
-          data={this.state.aptitudes}
+          dataDynmic={this.state}
+          dataStatic={this.staticData}
           handleChange={(event, keyOne, keyTwo ) => this.handleInputChange(event, 'aptitudes', keyOne, keyTwo)}
         />
         <br/>
-        <Skill
+        <Skills
           dataDynmic={this.state}
           dataStatic={this.staticData}
-          handleChange={(event, skill) => this.handleInputChange(event, 'skills', skill)}
+          handleChange={(event, skill, subSkill) => {
+            this.handleInputChange(event, 'skills', skill, subSkill)
+          }}
+          handleNewSubSkill={this.handleNewSubSkill}
         />
         <br/>
         <button onClick={()=>{
